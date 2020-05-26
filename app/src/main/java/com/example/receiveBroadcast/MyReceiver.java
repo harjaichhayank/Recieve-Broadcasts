ab21509996e028ff2caa43606e8e063ccd54ed2d
Chhayank <harjaichhayank1@gmail.com>
2020-05-27 03:58:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/receiveBroadcast/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.receiveBroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver(){
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"Message Received", Toast.LENGTH_SHORT).show();
+    }
+}
